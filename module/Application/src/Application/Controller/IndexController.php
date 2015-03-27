8fe1021c42d714f55ac99eb928afb0151c8731a9
surjur <262377509@qq.com>
2015-03-27 15:19:16 +0800

    add zip class

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -18,7 +18,26 @@ class IndexController extends AbstractActionController
     {
       // phpinfo();
  	 date_default_timezone_set('PRC');
-	 echo 'Hello Surjur <<<< '.date('Y-m-d').' >>>>';
-	 exit;
+	// echo 'Hello Surjur <<<< '.date('Y-m-d').' >>>>';
+	// exit;
+	//echo LIB;exit;
+	$host = '121.40.166.99';
+	$username = 'hyu1486730001';
+	$password = '13615734424';
+	$conn_id = ftp_connect($host) or die('connet false');
+	$login_result = ftp_login($conn_id, $username, $password);
+	ftp_chdir($conn_id, 'htdocs');
+
+	include_once(LIB.'/plugs/ZipArchive.class.php');
+	$za = new ZipArchive(); 
+	$za->open('/home/theZip.zip'); 
+	for( $i = 0; $i < $za->numFiles; $i++ ){ 
+    		$stat = $za->statIndex( $i ); 
+    		print_r( baename( $stat['name'] ) . PHP_EOL ); 
+	}
+	echo ftp_pwd($conn_id);
+	
+	ftp_close($conn_id);
+	exit;
     }
 }
